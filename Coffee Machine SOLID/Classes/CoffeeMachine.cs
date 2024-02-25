50bae671fc2fcb5a8d901f3a048f6543336f4a66
PandaOnCaffeine <Tuepvm@gmail.com>
2024-02-25 16:51:08 +0100

    Researched more about Open/Closed to better understand it so i remade part one

--- Coffee Machine SOLID/Classes/CoffeeMachine.cs
+++ Coffee Machine SOLID/Classes/CoffeeMachine.cs
@@ -1,81 +1,39 @@
 ﻿using Coffee_Machine_SOLID.AbstractClasses;
-using Coffee_Machine_SOLID.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Coffee_Machine_SOLID.Classes
 {
-    internal class CoffeeMachine : Machine, ICoffeeMachine
+    internal class CoffeeMachine
     {
-        private bool _filter = false;
-        private int _water = 0;
-        private Powder _powder;
-
+        private int _water;
         public CoffeeMachine()
         {
-            _powder = new CoffeePowder();
+            _water = 0;
         }
-        public override void Start()
+        public void AddWater(int amount)
         {
-            if (!_isOn)
-            {
-                Console.WriteLine("Coffee Machine Not Turn On");
-                return;
-            }
-
-            if (!_filter)
-            {
-                Console.WriteLine("No Filter");
-                return;
-            }
-
-            if (_water <= 0)
-            {
-                Console.WriteLine("Not Any Water Left");
-                return;
-            }
-
-            if (_powder.Amount <= 0)
-            {
-                Console.WriteLine($"Not Any {_powder.Name} Left");
-                return;
-            }
-            Brew();
-        }
-        public void Brew()
-        {
-            int count = 1;
-            while (_water >= 1 && _powder.Amount >= 1)
-            {
-                Console.WriteLine($"Cup nr {count} of coffee");
-                Thread.Sleep(400); //Wait time between each cup of coffee
-                _water -= 1;
-                _powder.Amount -= 1;
-                count++;
-            }
-            Console.WriteLine("Done");
-            _filter = false;
+            _water += amount;
+            Console.WriteLine($"Added {amount} cups of water to the machine.");
         }
 
         public void AddNewFilter()
         {
-            _filter = true;
+            Console.WriteLine($"Added a filter to the machine.");
+
         }
-        public void AddWater()
+
+        public void Brew(Beverage beverage)
         {
-            Console.WriteLine("How Many Cups worth of Water do you want add?");
-            int amount = Convert.ToInt32(Console.ReadLine());
-            _water += amount;
-        }
-        public void AddPowder()
-        {
-            Console.WriteLine("How Many Cups worth of Powder do you want add?");
-            int powderAmount = Convert.ToInt32(Console.ReadLine());
-            _powder.Amount += powderAmount;
+            if (_water >= beverage.Amount)
+            {
+                beverage.AddIngredients();
+                beverage.Brew();
+                _water -= beverage.Amount;
+            }
+            else
+            {
+                Console.WriteLine("Not enough water to brew. Please add water.");
+            }
         }
     }
 }

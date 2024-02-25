a9e73f4dd77f2b83f5bd179cb659a38d61648b81
PandaOnCaffeine <Tuepvm@gmail.com>
2024-02-25 16:56:45 +0100

    Parttwo

---
+++ Coffee Machine SOLID/Classes/Tea.cs
@@ -0,0 +1,24 @@
+﻿using Coffee_Machine_SOLID.AbstractClasses;
+using System;
+
+namespace Coffee_Machine_SOLID.Classes
+{
+    internal class Tea : Beverage
+    {
+        public Tea(int cups) : base(cups)
+        {
+
+        }
+
+        public override void AddIngredients()
+        {
+            Console.WriteLine("Added tea leaves to the machine.");
+        }
+
+        public override void Brew()
+        {
+            Console.WriteLine("Brewing tea...");
+            Console.WriteLine("Your tea is ready!");
+        }
+    }
+}
--- Coffee Machine SOLID/Program.cs
+++ Coffee Machine SOLID/Program.cs
@@ -11,10 +11,14 @@ namespace Coffee_Machine_SOLID
             CoffeeMachine coffeeMaker = new CoffeeMachine();
 
             // Make coffee
-            coffeeMaker.AddWater(2); // Add water for 2 cups
+            coffeeMaker.AddWater(2); // Add water for 2 cups of coffe
             Beverage coffee = new Coffee(2);
             coffeeMaker.Brew(coffee); // Brew coffee
 
+            // Make tea
+            coffeeMaker.AddWater(1); // Add water for 1 cups of tea
+            Beverage tea = new Tea(1);
+            coffeeMaker.Brew(tea); // Brew coffee
 
             Console.ReadLine();
         }
